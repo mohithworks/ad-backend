5d9b4c1121e51629bb34bc5facad190de42b52eb
mohithworks <mohithworks2002@gmail.com>
2023-04-30 18:06:59 +0530

    Changes

--- index.js
+++ index.js
@@ -89,6 +89,79 @@ app.get("/video/request", async function (req, res, next) {
 
 });
 
+app.get("/video/request2", async function (req, res, next) {
+ const userid = req.query.userid;
+ const campid = req.query.campid.toString();
+
+ console.log(campid)
+
+ sbSelectDefault('campaigns', 'source', 'id', campid).then(({error, data}) => { 
+  if(error) {
+    throw new Error(error.message);
+  }
+  if(data) {
+    var videoSource = data[0].source;
+    //Get video starting time
+    var startTime = new Date().toLocaleString(); 
+
+    //Spliting time to get maxTime
+    var time = startTime.split(' ');
+    var date = time[0];
+    var timeSplit = time[1].split(':');
+
+    //Getting maxTime
+    var maxMinute = (parseInt(timeSplit[1]) + 3).toString();
+    var maxTime = date + ' ' + timeSplit[0] + ':' + maxMinute + ':' + timeSplit[2];
+
+    var insertData = [{
+      userid: userid,
+      starttime: startTime,
+      maxtime: maxTime,
+    }]
+
+    //Checking user eligibility
+    sbSelectDefault(campid, 'userid, maxattempts', 'userid', userid).then(({error, data}) => { 
+      if(error) {
+        throw new Error(error.message);
+      }
+      if(data.length === 0) {
+        //Inserting record into campaign id table
+        console.log('Inserting');
+        sbInsert(campid, insertData).then(({error, data}) => {
+          if(error) {
+            throw new Error(error.message);
+          }
+          if(data) {
+            res.send(videoSource)
+          }
+        });  
+
+      }else if(data) {
+        console.log('Updating');
+        const { maxattempts } = data[0];
+        if(maxattempts !== 0) { 
+          //If user is eligible update the existing record
+          insertData[0]['maxattempts'] = maxattempts - 1;
+          sbUpdate(campid, insertData, 'userid', userid).then(({error, data}) => {
+            if(error) {
+              throw new Error(error.message);
+            }
+            if(data) {
+              res.send(videoSource)
+            }
+          });
+
+        } else {
+          res.status(400).send('You have exceeded the limit of viewing this video ad');
+        }
+    
+      }
+     })
+  }
+ })
+
+});
+
 
 app.get("/video/finish", async function (req, res, next) {
   const email = req.query.name;
@@ -177,6 +250,93 @@ app.get("/video/finish", async function (req, res, next) {
       
     }else {
       res.send('It was found that you haven\'t watched the video for 1 minute. You will not be rewarded for this ad')
+    }
+ 
+     
+   }
+  })
+ 
+});
+
+
+app.get("/video/finish2", async function (req, res, next) {
+  const userid = req.query.userid;
+  const campid = req.query.campid.toString();
+ 
+  console.log(campid)
+ 
+  sbSelectDefault(campid, 'starttime, maxtime', 'userid', userid).then(({error, data}) => { 
+    console.log('1')
+   if(error) {
+    throw new Error(error.message);
+   }
+   if(data) {
+     //Get video starting time
+     var startTime = data[0].starttime; 
+
+     //Spliting startTime
+     var timeS = startTime.split(' ');
+     var timeSplitS = timeS[1].split(':');
+
+     var videoMin = parseInt(timeSplitS[1]) + 1;//54
+ 
+     //Getting maxTime
+     var maxTime = data[0].maxtime;
+
+     //Spliting maxTime
+     var timeM = maxTime.split(' ');
+     var dateM = timeM[0];
+     var timeSplitM = timeM[1].split(':');
+
+     var videoMax = parseInt(timeSplitM[1]);//57
+
+     var endTime = new Date().toLocaleString();
+
+     //Spliting endTime
+     var timeE = endTime.split(' ');
+     var dateE = timeE[0];
+     var timeSplitE = timeE[1].split(':');
+
+     var videoCurrent = parseInt(timeSplitE[1]);
+     console.log('S: ' + videoMin + ' E: ' + videoMax + ' C: ' + videoCurrent)
+
+    //Checking if user has watched the video for 3 minutes
+    if(dateE === dateM && timeSplitE[0] === timeSplitM[0] && _.inRange(videoCurrent, videoMin, videoMax)) { 
+      console.log('User has watched the video for 1 minute');
+      sbSelectDefault('campaigns', 'interactions', 'id', campid).then(({error, data}) => { 
+        if(error) {
+          throw new Error(error.message);
+        }
+        if(data) {
+          var updatedInteractions = data[0].interactions + 1;
+          var updateData = [{
+            interactions: updatedInteractions,
+          }]
+          var updateData2 = [{
+            watch_stat: 'W'
+          }]
+          //Updating coins in campaigns table
+          sbUpdate('campaigns', updateData, 'id', campid).then(({error, data}) => {
+            if(error) {
+              throw new Error(error.message);
+            }
+            if(data) {
+              //Updating coins earned in campaign id table
+              sbUpdate('users', updateData2, 'id', userid).then(({error, data}) => {
+                if(error) {
+                  throw new Error(error.message);
+                }
+                if(data) {
+                  res.send('We are thankfull for watching the video!!')
+                }
+              });
+            }
+          })
+        }
+     })
+      
+    }else {
+      res.send('It was found that you haven\'t watched the video for 1 minute.')
     }
  
      
